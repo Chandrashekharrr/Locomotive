fc25ea4374163ad8a14656af0beb1db1fa7ceb5e
Chandrashekhar Chavan <chandrashekharcorp@gmail.com>
2025-02-04 13:57:22 +0530

    half part done for locomotive

--- app/page.js
+++ app/page.js
@@ -1,101 +1,85 @@
-import Image from "next/image";
+"use client";
+import React from 'react'
+import localFont from "next/font/local";
+import LocomotiveScroll from 'locomotive-scroll';
+import { FaArrowDownLong } from "react-icons/fa6";
 
-export default function Home() {
+const neue = localFont({
+  src: '../app/fonts/neueBold.otf',
+});
+const Neuemedium = localFont({
+  src: '../app/fonts/Neuemedium.otf',
+});
+const NeueLight = localFont({
+  src: '../app/fonts/NeueLight.otf',
+});
+
+const page = () => {
+
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='bg-[#d4c9c9] w-full  p-5 md:px-10 md:py-7'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className=" nav flex justify-between uppercase text-base md:text-2xl">
+        <h1 className={`${neue.className}`}>Locomotive.ca</h1>
+        <h1 className={`${neue.className}`}>v4.x – Edition</h1>
+      </div>
+
+      <div className='w-full text-[12vw] md:text-[13vw] uppercase leading-[1.2] md:leading-none mt-28 md:mt-20'>
+        <h1 className={`${Neuemedium.className}`}>Locomotive</h1>
+        <h1 className={`${Neuemedium.className}`}>Scroll</h1>
+
+        <div className={`${Neuemedium.className}   flex text-white `}>
+          <h1>v</h1>
+          <h1>4</h1>
+          <h1>.</h1>
+          <h1>x</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
+
+      <div className='w-full h-[1px] bg-black'></div>
+
+      <div className="parent w-full md:flex md:mt-10 md:items-center mt-10">
+        <div className='text-sm md:text-xl  w-full md:w-1/2 '>
+          <p className={`${Neuemedium.className}`}>A simple scroll library used by developers at Locomotive. Built as a layer on top of ayamflow's virtual-scroll, it provides smooth scrolling with support for parallax effects, toggling classes, and triggering event listeners when elements are in the viewport.</p>
+        </div>
+
+        <div className={`w-full mt-10 md:bg-green-500  gap-5 md:w-1/2 md:text-xl ${neue.className}`}>
+          <div className='flex justify-between uppercase '>
+            <h1>01. Speed Control</h1>
+            <FaArrowDownLong />
+          </div>
+          <div className='w-full h-[1px] bg-black'></div>
+
+          <div className='flex justify-between uppercase'>
+            <h1>02. Scroll direction</h1>
+            <FaArrowDownLong />
+          </div>
+          <div className='w-full h-[1px] bg-black'></div>
+
+          <div className='flex justify-between uppercase'>
+            <h1>03. Lerp effect</h1>
+            <FaArrowDownLong />
+          </div>
+          <div className='w-full h-[1px] bg-black'></div>
+
+          <div className='flex justify-between uppercase'>
+            <h1>04. Fixed Elements</h1>
+            <FaArrowDownLong />
+          </div>
+          <div className='w-full h-[1px] bg-black'></div>
+
+
+
+        </div>
+
+      </div>
+
+
     </div>
-  );
+  )
 }
+
+export default page
