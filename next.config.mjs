bbf94bae2963c33934b56bc6bee688355037a96d
Chandrashekhar Chavan <chandrashekharcorp@gmail.com>
2025-02-19 08:50:17 +0530

    ....

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    output: "export", // Enables static export
+  output: 'export', // Enables static export
   basePath: "/Locomotive", // Set this to your GitHub repo name
   images: {
     unoptimized: true, // Necessary because GitHub Pages does not support Next.js image optimization
